7c459fd9972d273a6b69903c298e02115949f926
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-19 23:10:04 +0100

    styles in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import style from "./style.css"
+import { Container, AppBar, Typography, Grid, Grow} from '@material-ui/core';
+import memories from './images/sroad.png'
+
 const App = () => {
     return(
-        <div>
-            <h1>the social media app</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant='h2' align="center">My Memories</Typography>
+                <img src={memories} alt='memories' height='60' />
+            </AppBar>
+        </Container>
     );
 }
 
