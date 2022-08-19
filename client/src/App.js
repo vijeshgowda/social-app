551d24c17c97225162953ccd8b90d3e3b3b5d5c6
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-19 19:17:21 +0100

    hi

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./style.css"
+const App = () => {
+    return(
+        <div>
+            <header>the social media app</header>
+        </div>
+    );
+}
+
+export default App;
