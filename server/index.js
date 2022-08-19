7029d9f78c6b6c245c280d5d11cc7975e650c12c
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-19 19:31:26 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,3 +8,4 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+const CONNECTION = "mongodb+srv://vijesh:nissangtr@test.vsfmhnv.mongodb.net/?retryWrites=true&w=majority"
