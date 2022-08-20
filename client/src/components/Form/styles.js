3167a10c63a63c58ba41ab4bd4fe3293ba6c37d2
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-20 23:00:53 +0100

    working form
    redux working
    everything works up to 1:09:13

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -13,6 +13,7 @@ export default makeStyles((theme) => ({
         display: 'flex',
         flexWrap: 'wrap',
         justifyContent: 'center',
+        gap: 15,
     },
     fileInput: {
         width: '97%',
