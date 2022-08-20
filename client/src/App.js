e048f788375ee2756a6645039f46aabaee956d0b
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-20 02:24:53 +0100

    files set to proper locations

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { Container, AppBar, Typography, Grid, Grow} from '@material-ui/core';
+
 import memories from './images/sroad.png'
+import Posts from "./components/Posts/Post/Post";
+import Form from "./components/Form/Form";
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return(
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant='h2' align="center">My Memories</Typography>
-                <img src={memories} alt='memories' height='60' />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant='h2' align="center">My Memories</Typography>
+                <img className={classes.image} src={memories} alt='memories' height='60' />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent={'space-between'} alignItems={'stretch'} spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
