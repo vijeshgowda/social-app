1782732ee71db1b633b6cacba96813f24a56b16e
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-21 18:42:27 +0100

    no idea

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/Posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vijesh:nissangtr@test.vsfmhnv.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://vijesh:dosaidlivada@test.vsfmhnv.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
